90934387911c48c2e63617aa55df16eb898b4e15
MeMurilo <160651102+MeMurilo@users.noreply.github.com>
2024-06-28 11:01:02 -0300

    Add files via upload

---
+++ Lista06/Ex05.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.AccessControl;
+using System.Xml;
+
+class ex05{
+static void Main(){
+
+string arquivo=@"J:\Lista 6\ex05.txt.txt";
+
+if(File.Exists(arquivo)){
+    string[] linhas= File.ReadAllLines(arquivo);
+
+   foreach(string linha in linhas){
+     Console.WriteLine(linha);
+   }
+
+   
+
+
+}
+int qlinhas= ContadorLinhas(arquivo);
+Console.WriteLine($"Quantidade de linhas: {qlinhas}");
+
+
+}
+
+static int ContadorLinhas(string arquivo){
+    string []linhas= File.ReadAllLines(arquivo);
+
+    return linhas.Length;
+ }
+}
