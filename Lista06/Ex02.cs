60c3f3945f04294cfdf9a687b551b11417df42e5
MeMurilo <160651102+MeMurilo@users.noreply.github.com>
2024-06-28 09:19:43 -0300

    Add files via upload

---
+++ Lista06/Ex02.cs
@@ -0,0 +1,29 @@
+﻿class ex02
+{
+   static void Main(){
+  Console.WriteLine("Digite sua frase");
+  string frase = Console.ReadLine();
+  string notvogais= Separador(frase);
+
+Console.WriteLine($"A frase sem vogais é {notvogais}");
+   }  
+  
+  static string Separador(string frase){
+   string vogais = "aeiouAEIOU";
+   string semvogais = "";
+   foreach(char c in frase){
+    if(!vogais.Contains(c)){
+        semvogais +=c;
+    }
+   }
+return semvogais;
+  }
+
+
+
+
+
+
+
+
+}
